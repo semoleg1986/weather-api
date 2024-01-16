48b800eb3cfe4b70ba597f6feb9ef09abca2c6ac
semoleg1986 <semoleg1986@gmail.com>
2024-01-16 21:38:27 +0400

    feat: impl. Weather model

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Weather(models.Model):
+    city = models.CharField(max_length=255, unique=True)
+    temperature = models.FloatField()
+    pressure = models.FloatField()
+    wind_speed = models.FloatField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)  
+
+    def __str__(self):
+        return self.city
